29217fc00fd362651783e3cdaa9e4d4d238641d8
Charles Arciniega <charlesarcin@gmail.com>
2014-06-23 17:52:37 -0700

    stubs the api with default info for some get routes

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-var http = require('http');
+var express = require('express');
 
-var server = http.createServer(function(req, res){
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World');
+var app = express();
+
+app.get('/wines', function(req, res) {
+    res.send([{name:'wine1'}, {name:'wine2'}]);
+});
+app.get('/wines/:id', function(req, res) {
+    res.send({id:req.params.id, name: "The Name", description: "description"});
 });
 
-server.listen(8000);
+app.listen(3000);
+console.log('Listening on port 3000...');
