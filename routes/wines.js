db52551d4dc1c6fc5c42dc9c8b76d40d9d1b0ac4
Charles Arciniega <charlesarcin@gmail.com>
2014-06-24 14:48:16 -0700

    adds mongodb configuration

--- routes/wines.js
+++ routes/wines.js
@@ -1,3 +1,14 @@
+// bring in mongo db
+var mongo = require('mongodb');
+
+var Server = mongo.Server,
+    Db = mongo.db,
+    BSON = mongo.BSONPure;
+
+// configure database server
+var server = new Server('localhost', 27017, {auto_reconnect: true}),
+    db = new Db('winedb', server);
+
 exports.findAll = function(req,res){
   res.send([{name: "wine1"}, {name: "wine2"}, {name: "wine3"}]);
 };
