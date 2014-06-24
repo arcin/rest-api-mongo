9734d6720187b800a1c4854629470a17c091f440
Charles Arciniega <charlesarcin@gmail.com>
2014-06-24 15:11:50 -0700

    adds sample data

--- routes/wines.js
+++ routes/wines.js
@@ -40,3 +40,32 @@ exports.findById = function(req,res){
     }});
   });
 };
+
+/********************************************************************************************************
+Sample Data
+*********************************************************************************************************/
+var populateDB = function() {
+  var wines = [
+  {
+    name: "CHATEAU DE SAINT COSME",
+    year: "2009",
+    grapes: "Grenache / Syrah",
+    country: "France",
+    region: "Southern Rhone",
+    description: "The aromas of fruit and spice...",
+    picture: "saint_cosme.jpg"
+  },
+  {
+    name: "LAN RIOJA CRIANZA",
+    year: "2006",
+    grapes: "Tempranillo",
+    country: "Spain",
+    region: "Rioja",
+    description: "A resurgence of interest in boutique vineyards...",
+    picture: "lan_rioja.jpg"
+  }];
+
+  db.collection('wines', function(err, collection) {
+    collection.insert(wines, {safe:true}, function(err, result) {});
+  });
+};
