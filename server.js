b6131e92723675f8964f50466be459d6ceb74362
Charles Arciniega <charlesarcin@gmail.com>
2014-06-24 14:33:34 -0700

    adding more crud routes

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ var express = require('express'),
 
 var app = express();
 
+// configuration
 app.use(bodyParser());
 app.use(morgan('dev'));
 
 app.get('/wines', wines.findAll);
 app.get('/wines/:id', wines.findById);
+app.put('/wines/:id', wines.updateWine);
+app.post('/wines', wines.addWine);
+app.delete('/wines/:id', wines.deleteWine);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
