3600e3a7f2d2c6b94be9977fe5bca50b45070196
Charles Arciniega <charlesarcin@gmail.com>
2014-06-27 14:40:04 -0700

    adds ability to delete wine

--- routes/wines.js
+++ routes/wines.js
@@ -73,6 +73,20 @@ exports.updateWine = function(req, res){
     });
   });
 };
+exports.deleteWine = function(req, res) {
+    var id = req.params.id;
+    console.log('Deleting wine: ' + id);
+    db.collection('wines', function(err, collection) {
+        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred - ' + err});
+            } else {
+                console.log('' + result + ' document(s) deleted');
+                res.send(req.body);
+            }
+        });
+    });
+};
 /********************************************************************************************************
 Sample Data
 *********************************************************************************************************/
